9d2341da2efdd836747c7e187ecb478f02af2b5e
yassin <yassin_123@hotmail.co.uk>
2025-12-16 13:22:18 +0000

    modified schedule on promotion pipeline

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 with DAG(
     dag_id="test_dag",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2024, 2, 1),
     schedule=None,
     catchup=False,
 ) as dag:
